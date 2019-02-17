f433954ded4dcef28c3e7694d3e0caaa65bc1ceb
Stone1994G <45983286+Stone1994G@users.noreply.github.com>
2019-02-17 13:18:57 -0700

    3.0 version of code

--- CSE360/src/cse360assign2/calculater.java
+++ CSE360/src/cse360assign2/calculater.java
@@ -8,32 +8,34 @@ package cse360assign2;
  * perform basic arithmetic operations
  * 
  * @author Stone Gulliksen
- * @version 1.0 (initial version)
+ * @version 3.0 
  * @since 02/16/2019
  */
 public class calculater {
 	
 	
 	/* primitive integer value to represent the total returned
-	 * within the calculations performed in the following functions
+	 * within the calculations performed in the following functions.
+	 * primitive String named history to represent all calculations performed 
 	 */
 	private int total;
+	private String history;
 	
 	/**
 	 * This constructor represents the calculator 
 	 * with the total integer variable being initialized to 
-	 * zero within it
+	 * zero within it, the history starts with a zero within its string
 	 */
 	public calculater () 
 	{
-		total = 0;  // not needed - included for clarity
+		total = 0;  
+		history = "0";
 	}
 	
 	/**
-	 * 
 	 * This method getTotal is used to return the integer
 	 * value that is represented by variable 'total'
-	 * @return 0 integer being returned from function at time
+	 * @return total being returned from function at time
 	 */
 	public int getTotal () 
 	{
@@ -43,13 +45,13 @@ public class calculater {
 	/**
 	 * This is the add method that will perform the arithmetic operation
 	 * Addition which will increase the total value by the amount of the integer
-	 * passed in
-	 * @param integer value being passed to be added onto the total
+	 * being passed into the function
+	 * @param integer value being passed in to be added onto the total
 	 */
 	public void add (int value)
 	{
 		total += value;
-		
+		history = history.concat(" + " + value);
 	}
 	
 	/**
@@ -61,6 +63,7 @@ public class calculater {
 	public void subtract (int value) 
 	{
 		total -= value;
+		history = history.concat(" - " + value);
 	}
 	
 	/**
@@ -72,6 +75,7 @@ public class calculater {
 	public void multiply (int value) 
 	{
 		total *= value;
+		history = history.concat(" * " + value);
 	}
 	
 	/**
@@ -81,6 +85,7 @@ public class calculater {
 	 */
 	public void divide (int value)
 	{
+		history = history.concat(" / " + value);
 		if(value == 0)
 			total = 0;
 		else
@@ -94,6 +99,6 @@ public class calculater {
 	 */
 	public String getHistory ()
 	{
-		return "";
+		return history;
 	}
 }
